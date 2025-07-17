4c769721a13fb5a0eec634642f6e38bd500783ed
leonnoel <leon.a.noel+github@gmail.com>
2025-07-17 15:28:13 -0700

    simple appwrite setup with login, env vars, and docs

---
+++ src/app/appwrite.ts
@@ -0,0 +1,10 @@
+import { Client, Account, ID } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!) // Your API Endpoint
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!); // Your project ID
+
+export const account = new Account(client);
+export { ID } from 'appwrite';
