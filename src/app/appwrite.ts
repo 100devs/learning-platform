6356bb06a15129d2659c9d8aecca6ff6bede2c0d
leonnoel <leon.a.noel+github@gmail.com>
2025-07-20 01:08:51 -0700

    fix ts errors for appwrite deployment

--- src/app/appwrite.ts
+++ src/app/appwrite.ts
@@ -3,8 +3,8 @@ import { Client, Account} from 'appwrite';
 const client = new Client();
 
 client
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT) // Your API Endpoint
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID); // Your project ID
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!) // Your API Endpoint
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!); // Your project ID
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
